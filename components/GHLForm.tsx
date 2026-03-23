fd63ebac0f2da26b38be4843218d0956869efd2d
Alfred Marktr <alfred@Mac-mini.station>
2026-03-23 16:07:35 +0100

    Replace lead form with GHL survey on optin page

--- components/GHLForm.tsx
+++ components/GHLForm.tsx
@@ -1,38 +1,24 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import Script from "next/script";
 
 export default function GHLForm() {
-  const router = useRouter();
-
   useEffect(() => {
-    const handleMessage = (e: MessageEvent) => {
-      if (e.data && e.data.type === "form-submitted") {
-        router.push("/book");
-      }
-      // Some GHL versions send different event types
-      if (e.data && (e.data.event === "form-submitted" || e.data.formSubmitted)) {
-        router.push("/book");
-      }
+    const script = document.createElement("script");
+    script.src = "https://book.marktr.co/js/form_embed.js";
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
     };
-
-    window.addEventListener("message", handleMessage);
-    return () => window.removeEventListener("message", handleMessage);
-  }, [router]);
+  }, []);
 
   return (
-    <div className="w-full">
-      <iframe
-        src="https://link.msgsndr.com/widget/form/4Xa72iJ6Q1EuDP30xa1N"
-        style={{ width: "100%", border: "none", overflow: "hidden", minHeight: "480px" }}
-        scrolling="no"
-        id="msgsndr-form"
-        allow="camera; microphone; autoplay; payment"
-        title="Book Your Free Consultation"
-      />
-      <Script src="https://link.msgsndr.com/js/form_embed.js" strategy="lazyOnload" />
-    </div>
+    <iframe
+      src="https://book.marktr.co/widget/survey/y767vKIB2bpWma1lJIXR"
+      style={{ border: "none", width: "100%", minHeight: "600px" }}
+      scrolling="no"
+      id="y767vKIB2bpWma1lJIXR"
+      title="survey"
+    />
   );
 }
