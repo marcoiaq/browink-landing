6f7c27ec3152413ec448efcde4c0f12989460ba0
BrowInk Deploy <deploy@browink.co>
2026-03-29 15:29:03 +0200

    Update survey embed: match exact GHL iframe style (no forced minHeight)

--- components/GHLForm.tsx
+++ components/GHLForm.tsx
@@ -3,22 +3,16 @@
 import { useEffect } from "react";
 
 export default function GHLForm() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://book.marktr.co/js/form_embed.js";
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
-    <iframe
-      src="https://book.marktr.co/widget/survey/y767vKIB2bpWma1lJIXR"
-      style={{ border: "none", width: "100%", minHeight: "600px" }}
-      scrolling="no"
-      id="y767vKIB2bpWma1lJIXR"
-      title="survey"
-    />
+    <>
+      <iframe
+        src="https://book.marktr.co/widget/survey/y767vKIB2bpWma1lJIXR"
+        style={{ border: "none", width: "100%" }}
+        scrolling="no"
+        id="y767vKIB2bpWma1lJIXR"
+        title="survey"
+      />
+      <script src="https://book.marktr.co/js/form_embed.js" />
+    </>
   );
 }
